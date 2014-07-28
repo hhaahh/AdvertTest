504af1e7d00d12778a6dcd91c30670c4bcf6c919
yuriy <belyakov.u@gmail.com>
2014-07-28 16:48:37 +0400

    angularJS

--- js/controllers/NewsController.js
+++ js/controllers/NewsController.js
@@ -1,6 +1,12 @@
 function NewsController($scope, $http) {
     $scope.news = [];
 
+    $scope.orderOptions = {
+        field: 'created_date'
+    };
+
+    $scope.reverseOptions = {};
+
     $scope.getNews = function() {
         $http({
             url: '/news/getNews',
@@ -11,5 +17,16 @@ function NewsController($scope, $http) {
                 $scope.news.push(newsItem);
             });
         });
+    }
+
+    $scope.setOrder = function(field, reverse) {
+        if ($scope.orderOptions.field != field) {
+            $scope.reverseOptions[field] = reverse;
+        } else {
+            $scope.reverseOptions[field] = !$scope.reverseOptions[field];
+        }
+        $scope.orderOptions.field = field;
+
+        return false;
     }
 }
---
+++ protected/data/dump.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.52 (64 bit)
+MySQL - 5.5.38-0ubuntu0.14.04.1 : Database - at
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`at` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `at`;
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `text` text NOT NULL,
+  `like_count` int(11) NOT NULL DEFAULT '0',
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `news` */
+
+insert  into `news`(`id`,`title`,`text`,`like_count`,`created_date`) values (1,'Новость 1','Текст 1',1,'2014-07-28 10:56:57'),(2,'Новость 2','Текст 2',4,'2014-07-28 10:57:08'),(3,'Новость 5','Текст 5',2,'2014-07-28 16:39:42');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
